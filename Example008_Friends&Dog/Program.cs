f9b4375d7cb4c2cb9492d7692eea3dd6f0090eee
Bronislav <shevchenko-bg@yandex.ru>
2023-09-17 18:51:20 +0300

    added Exampl 008 Friends&Dog

---
+++ Example008_Friends&Dog/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;
+int distance = 10000;
+int friend1 = 1;
+int friend2 = 2;
+int dog = 5;
+int friend = 2;
+int time = 0;
+
+while (distance > 10)
+{
+    friend = new Random().Next(1, 3); //1, 2.
+    if (friend == 1)
+    {
+        time = distance/(friend1 + dog);
+        friend = 2;
+    }
+    else
+    {
+        time = distance/(friend2 + dog);
+        friend = 1;
+    }
+    distance = distance - (friend1 + friend2) * time;
+    count++;
+}
+Console.Write("Вот сколько раз собака бегала между друзьями: ");
+Console.WriteLine(count);
