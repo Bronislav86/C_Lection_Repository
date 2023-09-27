3838dd673110c5d5f038ca827b6ef8472f7d7195
Bronislav <shevchenko-bg@yandex.ru>
2023-09-27 21:50:09 +0300

    format code in Eexample 013 Method

--- Example013_Methods/Program.cs
+++ Example013_Methods/Program.cs
@@ -93,52 +93,54 @@ Method1(); // вЫЗОВ МЕТОДА
 
 //Цикл в цикле
 
-    // string result = String.Empty; //тоже самое что result = "";
-    // for (int i = 2; i < 10; i++)
-    // {
-    //     for (int j = 0; j <=10; j++)
-    //     {
-    //         Console.WriteLine($"{i} x {j} = {i * j}");
-    //     }
-    //     Console.WriteLine();
-    // }
+// string result = String.Empty; //тоже самое что result = "";
+// for (int i = 2; i < 10; i++)
+// {
+//     for (int j = 0; j <=10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j} = {i * j}");
+//     }
+//     Console.WriteLine();
+// }
 
-    //------------------------------------------------------------------
+//------------------------------------------------------------------
 
-    //Используем цикл for: Дан текст, в нем нужно все пробелы заменить
-    // черточками, маленькие буквы "к" заменить большими "К", а большие "С" на маленькие "с"
+//Используем цикл for: Дан текст, в нем нужно все пробелы заменить
+// черточками, маленькие буквы "к" заменить большими "К", а большие "С" на маленькие "с"
 
-    //Ясна ли задача? Что за дан текст?? Ввел пользов, скачали, или  что? - У нас это будет строка
-    //Что значит черточками? Какими? Тире, дефис, минус.
-    //Какой алфавит?
+//Ясна ли задача? Что за дан текст?? Ввел пользов, скачали, или  что? - У нас это будет строка
+//Что значит черточками? Какими? Тире, дефис, минус.
+//Какой алфавит?
 
-    string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
-                + "ежели бы вас послали вместо нашего милого Винценгероде,"
-                + "вы бы взяли приступом согласие прусского короля. "
-                + "Вы так краснороечивы. Вы дидате мне чаю?";
-    // string s = "qwerty"
-    //             012345
-    //s[3] // r
+string text =
+    "- Я думаю, - сказал князь, улыбаясь, - что, "
+    + "ежели бы вас послали вместо нашего милого Винценгероде,"
+    + "вы бы взяли приступом согласие прусского короля. "
+    + "Вы так краснороечивы. Вы дидате мне чаю?";
 
-    // string Replace(string text, char oldValue, char newValue)
-    // {
-    //     string result = String.Empty;
+// string s = "qwerty"
+//             012345
+//s[3] // r
 
-    //     int length = text.Length;
-    //     for (int i = 0; i < length; i++)
-    //     {
-    //         if(text[i] == oldValue) result = result + $"{newValue}";
-    //         else result = result + $"{text[i]}";
-    //     }
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
 
-    //     return result;
-    // }
-    // string newText = Replace(text, ' ', '|');
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if(text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
 
-    // Console.WriteLine(newText);
-    // Console.WriteLine();
-    // newText = Replace(newText, 'к', 'К');
-    // Console.WriteLine(newText);
+//     return result;
+// }
+// string newText = Replace(text, ' ', '|');
+
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
 
 //----------------------------------------------------------------------------------
 
@@ -152,9 +154,9 @@ Method1(); // вЫЗОВ МЕТОДА
 //2. Произвести обмен этого значения со значением первой неотсортированной позиции
 //3. Повторять пока есть неотсортированные элементы
 
-int [] arr = {66, 81, 13, 12, 11, 14, 51, 17};
+int[] arr = { 66, 81, 13, 12, 11, 14, 51, 17 };
 
-void PrintArray(int [] array)
+void PrintArray(int[] array)
 {
     int count = array.Length;
 
@@ -165,15 +167,16 @@ void PrintArray(int [] array)
     Console.WriteLine();
 }
 
-void SelectionSort(int [] array)
+void SelectionSort(int[] array)
 {
-    for (int i = 0; i<array.Length-1; i++)
+    for (int i = 0; i < array.Length - 1; i++)
     {
         int minPosition = i;
-        
-        for (int j = i + 1; j<array.Length; j++)
+
+        for (int j = i + 1; j < array.Length; j++)
         {
-            if (array[j] < array[minPosition]) minPosition = j;
+            if (array[j] < array[minPosition])
+                minPosition = j;
         }
         int temporary = array[i];
         array[i] = array[minPosition];
